88d58d8ade78f046b33d15996928c57d760f02fc
DeMiro5001 <68397534+DeMiro5001@users.noreply.github.com>
2024-12-30 17:08:36 +0100

    Create plugin.py

---
+++ indico_payment_clictopay/plugin.py
@@ -0,0 +1,36 @@
+from indico.core.plugins import IndicoPlugin, IndicoPluginBlueprint
+from indico.modules.events.payment import PaymentEventSettingsProxy
+from indico.web.forms import IndicoForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired
+
+class ClicToPaySettingsForm(IndicoForm):
+    merchant_user = StringField('Merchant Username', validators=[DataRequired()])
+    merchant_password = StringField('Merchant Password', validators=[DataRequired()])
+    api_environment = SelectField('API Environment', choices=[('test', 'Test'), ('production', 'Production')], validators=[DataRequired()])
+
+    def populate_obj(self, obj):
+        super().populate_obj(obj)
+        if self.api_environment.data == 'test':
+            obj.api_base_url = 'https://test.clictopay.com/payment/rest'
+        elif self.api_environment.data == 'production':
+            obj.api_base_url = 'https://ipay.clictopay.com/payment/rest'
+
+class ClicToPayPlugin(IndicoPlugin):
+    """ClicToPay Payment Plugin"""
+
+    configurable = True
+    settings_proxy = PaymentEventSettingsProxy('payment_clictopay', {
+        'merchant_user': '',
+        'merchant_password': '',
+        'api_base_url': 'https://test.clictopay.com/payment/rest',  # Default to test environment
+        'api_environment': 'test',  # Default setting
+    })
+
+    def init(self):
+        super().init()
+        self.template_hook('event-management-sidemenu', 'payment_clictopay:sidemenu')
+
+    def get_settings_form(self):
+        return ClicToPaySettingsForm()
+
